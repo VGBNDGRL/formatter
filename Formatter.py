211760d94d33ece48370853b722d79c4fc6259f6
annaflores <annaflowersk@gmail.com>
2020-05-06 22:36:56 -0500

    Added parameters to init and created simple classmethod

--- Formatter.py
+++ Formatter.py
@@ -1,56 +1,17 @@
 # Formatter.py
-class Formatter:
-    'Formatter superclass for formatting single input lines'
-    # add class variables here
+# create a text formatter, which understands:
+# margins, left justification, right justification, centering, bullet lists, and variable substitution
 
-    # define constructor (notice the use of
-    # optional parameters instead of multiple
-    # constructors)
-    def __init__(self, currentworkingline):
+
+class Formatter:
+
+    def __init__(self, current_working_line, variable_dictionary, format_dictionary):
         # define instance variables
-        self.currentline= currentworkingline
+        self.current_line = current_working_line
+        self.variable_dictionary = variable_dictionary
+        self.format_dictionary = format_dictionary
 
     # define class methods
     @classmethod
-    def formatline(cls, inputline):
-        print("formatline")
-
-    # define instance methods
-
-    # # Employee.py
-    # class Employee:
-    #     'Employee superclass for all employees'
-    #     lastId = 0  # class variable
-    #
-    #     # define constructor (notice the use of
-    #     # optional parameters instead of multiple
-    #     # constructors)
-    #     def __init__(self, name, salary, prefix="", exempt=1):
-    #         # define instance variables
-    #         self.namePrefix = prefix
-    #         self.name = name
-    #         self.salary = salary
-    #         self.exemptions = exempt
-    #         self.id = Employee.generateID()
-    #
-    #     # define class methods
-    #     @classmethod
-    #     def generateID(cls):
-    #         if Employee.lastId == 0:
-    #             Employee.lastId = 111  # db.getLastId()
-    #         Employee.lastId += 1
-    #         return Employee.lastId
-
-    #     # define instance methods
-    #     def getFormattedName(self):
-    #         if self.namePrefix == "":
-    #             return self.name
-    #         else:
-    #             return self.namePrefix + " " + self.name
-    #
-    #     def calculatePay(self):
-    #         if self.exemptions == 1:
-    #             return self.salary / 12
-    #         else:
-    #             return self.salary / 12 * 0.9
-
+    def substituteWords(cls, input_line):
+        print("This is subtituteWords")
