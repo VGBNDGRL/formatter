8de985622b9fb60be5482666b2442c6a6ba52184
annaflores <annaflowersk@gmail.com>
2020-05-06 22:37:26 -0500

    Renamed variables to follow consistent naming conventions

---
+++ p5Driver.py
@@ -0,0 +1,58 @@
+#! /usr/bin/python3
+import sys
+import os
+import pprint
+from p4At import setformat, setvariable
+
+if len(sys.argv) < 2:
+    print("filename needed as command argument")
+    sys.exit(1)
+if not os.path.isfile(sys.argv[1]):
+    print("***file not found")
+    sys.exit(1)
+file = open(sys.argv[1], "r")
+
+# TODO: move dictionaries!
+variable_dictionary = {}
+format_dictionary = {
+    "FLOW": "YES"
+    , "LM": "1"
+    , "RM": "80"
+    , "JUST": "LEFT"
+    , "BULLET": "o"}
+
+
+def readCommands(file):
+    """
+    :param file: text file to be parsed
+    :return: N/A
+    """
+
+    while True:
+        input_line = file.readline()
+        if input_line == "":  # check for empty input
+            break
+        input_line = input_line.rstrip('\n')  # removes newline
+        token_list = input_line.split()
+
+        # go thru tokens if we have valid func calls
+        if token_list[0] == "@.":
+            if token_list[1] == "VAR":
+                setvariable(input_line[6:], variable_dictionary)
+            elif token_list[1] == "FORMAT":
+                token_list.remove("@.")
+                token_list.remove("FORMAT")
+                setformat(token_list, format_dictionary)
+            elif token_list[1] == "PRINT":
+                if token_list[2] == "VARS":
+                    pprint.pprint(variable_dictionary, width=30)
+                elif token_list[2] == "FORMAT":
+                    pprint.pprint(format_dictionary, width=30)
+            else:
+                print("*** Not a recognizable command, found:" + input_line)
+        else:
+            print("*** Not a recognizable command, found:" + input_line)
+
+
+readCommands(file)
+file.close()
